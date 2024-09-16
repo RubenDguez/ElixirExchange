99cad0442a57a67ebad19bde6ba22fc60a01693e
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-16 17:16:46 -0400

    add: missing users in seed db

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -2,6 +2,9 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
+    { username: 'JollyGuru', password: 'password' },
+    { username: 'SunnyScribe', password: 'password' },
+    { username: 'RadiantComet', password: 'password' },
     { username: 'RubenDominguez', password: 'password' },
     { username: 'ChrisCrumbley', password: 'password' },
     { username: 'LeonardoReis', password: 'password' }
