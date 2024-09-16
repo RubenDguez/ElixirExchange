97649d76230aeb9274d00ee3d13301a0d6f206c1
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2024-09-16 13:07:38 -0400

    remove: unnecessary files (#1)

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,7 +3,6 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
-import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
@@ -16,9 +15,5 @@ const sequelize = process.env.DB_URL
     });
 
 const User = UserFactory(sequelize);
-const Ticket = TicketFactory(sequelize);
 
-User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
-
-export { sequelize, User, Ticket };
+export { sequelize, User };
