88630e6c44e22a131dd322567304d656d3a55655
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-17 12:15:01 -0400

    add: terms and conditions input in signup form

--- client/src/components/Forms/SignUp/index.tsx
+++ client/src/components/Forms/SignUp/index.tsx
@@ -4,6 +4,7 @@ import { login } from '../../../api/authAPI';
 import { createUser } from '../../../api/userAPI';
 import Auth from '../../../utils/auth';
 import '../Login/login.css';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [error, setError] = useState('');
@@ -13,6 +14,7 @@ const SignUp = () => {
   const lastNameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const dobRef = useRef<HTMLInputElement>(null);
+  const tocRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = async (e: FormEvent) => {
@@ -49,27 +51,31 @@ const SignUp = () => {
         {error !== '' && <h4 className="form-error-message">{error}</h4>}
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} exit={{ opacity: 0 }}>
           <label htmlFor="username">Username</label>
-          <input ref={usernameRef} type="text" id="username" name="username" />
+          <input ref={usernameRef} type="text" id="username" name="username" required />
         </motion.div>
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.6 }} exit={{ opacity: 0 }}>
           <label htmlFor="password">Password</label>
-          <input ref={passwordRef} type="password" id="password" name="password" />
+          <input ref={passwordRef} type="password" id="password" name="password" required />
         </motion.div>
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.7 }} exit={{ opacity: 0 }}>
           <label htmlFor="first-name">First Name</label>
-          <input ref={firstNameRef} type="text" id="first-name" name="first-name" />
+          <input ref={firstNameRef} type="text" id="first-name" name="first-name" required />
         </motion.div>
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }} exit={{ opacity: 0 }}>
           <label htmlFor="last-name">Last Name</label>
-          <input ref={lastNameRef} type="text" id="last-name" name="last-name" />
+          <input ref={lastNameRef} type="text" id="last-name" name="last-name" required />
         </motion.div>
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9 }} exit={{ opacity: 0 }}>
           <label htmlFor="email">Email</label>
-          <input ref={emailRef} type="email" id="email" name="email" />
+          <input ref={emailRef} type="email" id="email" name="email" required />
         </motion.div>
         <motion.div className="form-field" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} exit={{ opacity: 0 }}>
           <label htmlFor="dob">Date of Birth</label>
-          <input ref={dobRef} type="date" id="dob" name="dob" style={{ fontFamily: 'Roboto' }} />
+          <input ref={dobRef} type="date" id="dob" name="dob" style={{ fontFamily: 'Roboto' }} required />
+        </motion.div>
+        <motion.div className="form-toc" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} exit={{ opacity: 0 }}>
+          <label htmlFor="toc">Must accept and acknowledge our <Link to='/terms'>terms and conditions</Link></label>
+          <input ref={tocRef} type="checkbox" id="toc" name="toc" required />
         </motion.div>
         <motion.div className="form-action" initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1.1 }} exit={{ opacity: 0 }}>
           <button type="submit">Submit</button>
