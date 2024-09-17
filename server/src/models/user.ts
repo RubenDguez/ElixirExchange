1f1e5d421f04d67a75c1d7ec42a4cf7eb85fa49d
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-16 21:51:17 -0400

    update: user table name to ElixirUser

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -42,7 +42,7 @@ export function UserFactory(sequelize: Sequelize): typeof User {
       },
     },
     {
-      tableName: 'users',
+      tableName: 'ElixirUser',
       sequelize,
       hooks: {
         beforeCreate: async (user: User) => {
