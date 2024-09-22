56672e1c4371b0bf262920caf17cdedcd59f67ad
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-21 20:48:53 -0400

    add: form error handling

--- client/src/pages/Landing/AddCategoryForm/index.tsx
+++ client/src/pages/Landing/AddCategoryForm/index.tsx
@@ -8,33 +8,45 @@ export default function AddCategoryForm() {
   const formRef = useRef<HTMLFormElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
   const [categories, setCategories] = useState<Array<{ id: number; name: string }>>([]);
+  const [error, setError] = useState('');
 
   const { getJwt } = useAuthenticate();
 
-  const handleDeleteCategory = useCallback(async(e: React.MouseEvent<SVGElement, MouseEvent>, id: number) => {
-    e.preventDefault();
-    try {
+  const handleDeleteCategory = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement, MouseEvent>, id: number) => {
+      e.preventDefault();
+      try {
         const response = await fetch(`/api/categories/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${getJwt()}`,
-            }
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${getJwt()}`,
+          },
         });
 
         const data = await response.json();
         if (data.message === 'Category deleted successfully') {
-            const filteredCat = categories.filter((f) => (f.id !== id));
-            setCategories(() => [...filteredCat]);
+          const filteredCat = categories.filter((f) => f.id !== id);
+          setCategories(() => [...filteredCat]);
         }
-    } catch (error) {
-        console.error(error);
-    }
-  }, [getJwt, categories]);
+      } catch (err) {
+        const ERROR = err as Error;
+        setError(ERROR.message);
+        console.error(ERROR.message);
+      }
+    },
+    [getJwt, categories],
+  );
 
   const handleSubmitCategory = useCallback(
     async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       e.preventDefault();
+
+      if (nameRef.current?.value === '') {
+        setError('Please fill in the name field');
+        nameRef.current.focus();
+        return;
+      }
 
       try {
         const response = await fetch('/api/categories', {
@@ -54,7 +66,10 @@ export default function AddCategoryForm() {
 
         throw new Error('Category already exists');
       } catch (err) {
-        console.error(err);
+        const ERROR = err as Error;
+        setError(ERROR.message);
+        nameRef.current?.focus();
+        console.error(ERROR.message);
       } finally {
         formRef.current?.reset();
       }
@@ -81,16 +96,19 @@ export default function AddCategoryForm() {
 
   return (
     <div className="add-category-form">
-      <div className="add-category-error">
-        <h3>This is an error</h3>
-      </div>
+      {error !== '' && (
+        <div className="add-category-error">
+          <h3>{error}</h3>
+        </div>
+      )}
       <form ref={formRef}>
         <div className="form-control">
           <label htmlFor="cat-name">Category Name</label>
-          <input ref={nameRef} type="text" name="cat-name" id="cat-name" />
+          <input required ref={nameRef} type="text" name="cat-name" id="cat-name" />
         </div>
         <div className="form-action">
           <button
+            type='submit'
             onClick={(e) => {
               handleSubmitCategory(e);
             }}
@@ -112,9 +130,11 @@ export default function AddCategoryForm() {
             {categories.length > 0 &&
               categories.map((cat) => (
                 <tr key={cat.id}>
+                  <td>{cat.name}</td>
                   <td>
-                    {cat.name}
-                    <TiDeleteOutline className="action-button" onClick={(e) => handleDeleteCategory(e, cat.id)} />
+                    <div onClick={(e) => handleDeleteCategory(e, cat.id)} className="action">
+                      <TiDeleteOutline className="action-button" />
+                    </div>
                   </td>
                 </tr>
               ))}
