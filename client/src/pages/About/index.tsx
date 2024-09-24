789bdc10e12ab178946fb93e9374283ed8f9fdea
reisleo99 <leonardo.ao.reis@gmail.com>
2024-09-23 21:01:47 -0400

    Adjusting About Page

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import rawAbout from '/about.md?url';
+import Markdown from 'react-markdown';
+import './about.css';
+
 export default function About() {
+    const [about, setAbout] = useState('');
+    useEffect(() => {
+        async function readFile() {
+          try {
+            const response = await fetch(rawAbout);
+            const text = await response.text();
+    
+            setAbout(text);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+    
+        readFile();
+      }, []);
     return (
-        <div>
-            <h2>This is the about page</h2>
+        <div className='about'>
+            <Markdown>{about}</Markdown>
         </div>
     )
 }
