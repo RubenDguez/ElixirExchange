fd3cdf61e446959cab210f532d5f1c2ba8ba375d
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-24 11:55:51 -0400

    update: shareable elixir page

--- client/src/api/categoryAPI.ts
+++ client/src/api/categoryAPI.ts
@@ -9,3 +9,15 @@ export async function getAllCategories(jwt: string): Promise<Array<{ id: number;
       const data: Array<{ id: number; name: string }> = await response.json();
       return data;
 }
+
+export async function getCategory(jwt: string, id: number): Promise<{ id: number; name: string }> {
+    const response = await fetch(`/drinks/cat/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+    
+      const data: { id: number; name: string } = await response.json();
+      return data;
+}
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,10 +3,12 @@ import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 import { signupRoutes } from './api/signup-routes.js';
+import { drinksRouter } from './drinks-routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/api', authenticateToken, apiRoutes);
-router.use('/signup', signupRoutes)
+router.use('/signup', signupRoutes);
+router.use('/drinks', drinksRouter);
 export default router;
