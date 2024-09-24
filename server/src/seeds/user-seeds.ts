e8c729f505203dce170d022c96c42d0ca10cad30
RealChillyPalmer <highvibes.dev@gmail.com>
2024-09-23 22:38:24 -0400

    added myself in seeds/Contact page (#26)
    * added myself in seeds
    
    * contact page and added myself as user
    
    * no footer

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,5 +3,6 @@ import { User } from '../models/user.js';
 export const seedUsers = async () => {
   await User.bulkCreate([
     { username: 'AutoBot', password: 'password', firstName: 'Auto', lastName: 'Bot', email: 'auto.bot@test.com', dob: '2024-09-16' },
+    { username: 'ChillyP', password: 'password', firstName: 'Chilly', lastName: 'Palmer', email: 'chilly.p@coolmail.com', dob:'1989'},
   ], { individualHooks: true });
 };
