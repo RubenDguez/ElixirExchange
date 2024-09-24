c2c10404baeec0512701f1f5e0465ac5aa0cce23
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-24 12:11:26 -0400

    update: shareable elixir page css

--- client/src/pages/ElixirExchange/index.tsx
+++ client/src/pages/ElixirExchange/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import './elixir.css';
+import Footer from '../../components/Footer';
 
 interface IElixir {
   name: string;
@@ -41,10 +42,12 @@ export default function ElixirExchange() {
   }, [id]);
 
   if (error) {
-    return <div className='elixir-error'>
-      <h1>Really? out of all the drinks in this site, you had to end up here...</h1>
-      <h1>{error}</h1>
-    </div>;
+    return (
+      <div className="elixir-error">
+        <h1>Really? out of all the drinks in this site, you had to end up here...</h1>
+        <h1>{error}</h1>
+      </div>
+    );
   }
 
   if (elixir === null) {
@@ -52,45 +55,48 @@ export default function ElixirExchange() {
   }
 
   return (
-    <div className="elixir-exchange">
-      <div className="elixir-inner">
-        <div className="elixir-header">
-          <div className='elixir-subheader'>
-            <div className="elixir-exchange-logo"></div>
-            <h2>Elixir Exchange</h2>
+    <div className='shareable-page'>
+      <div className="elixir-exchange">
+        <div className="elixir-inner">
+          <div className="elixir-header">
+            <div className="elixir-subheader">
+              <div className="elixir-exchange-logo"></div>
+              <h2>Elixir Exchange</h2>
+            </div>
+            <p>
+              Not a member yet? please <Link to="/">create an account</Link> and start sharing some of your best prepared drinks
+            </p>
           </div>
-          <p>
-            Not a member yet? please <Link to="/">create an account</Link> and start sharing some of your best prepared drinks
-          </p>
+          <h2 className="elixir-exchange-title">{elixir.name}</h2>
+          <div className="elixir-exchange-description">
+            <p>{elixir.description}</p>
+            <p>{elixir.category.name}</p>
+          </div>
+          <h3 className="elixir-exchange-table-header">Ingredients</h3>
+          <table className="elixir-exchange-table">
+            <thead>
+              <tr>
+                <th>name</th>
+                <th>unit</th>
+                <th>amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {elixir.ElixirIngredients.length > 0 &&
+                elixir.ElixirIngredients.map((ing) => (
+                  <tr key={ing.name}>
+                    <td>{ing.name}</td>
+                    <td>{ing.unit}</td>
+                    <td>{ing.amount}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <h3 className="elixir-exchange-table-header">Instructions</h3>
+          <p className="elixir-exchange-instructions">{elixir.instructions}</p>
         </div>
-        <h2 className="elixir-exchange-title">{elixir.name}</h2>
-        <div className="elixir-exchange-description">
-          <p>{elixir.description}</p>
-          <p>{elixir.category.name}</p>
-        </div>
-        <h3 className="elixir-exchange-table-header">Ingredients</h3>
-        <table className="elixir-exchange-table">
-          <thead>
-            <tr>
-              <th>name</th>
-              <th>unit</th>
-              <th>amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {elixir.ElixirIngredients.length > 0 &&
-              elixir.ElixirIngredients.map((ing) => (
-                <tr key={ing.name}>
-                  <td>{ing.name}</td>
-                  <td>{ing.unit}</td>
-                  <td>{ing.amount}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <h3 className="elixir-exchange-table-header">Instructions</h3>
-        <p className="elixir-exchange-instructions">{elixir.instructions}</p>
       </div>
+      <Footer />
     </div>
   );
 }
